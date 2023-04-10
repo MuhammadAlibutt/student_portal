8f5986ed64a1cbcafe255bd272ddc652f6630aa0
Muhammad Ali <muhammad.ali@appifytech.com>
2023-04-10 15:52:19 +0500

    aliarif

--- lib/home.dart
+++ lib/home.dart
@@ -7,29 +7,71 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Welcome on Board'),
+        title: Text('Find me Tutor!'),
+        centerTitle: true,
       ),
-      body: Container(
-        child: Column(children: [
-          Center(
-            child: Text('Kindly Select who you are'),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.admin_panel_settings),
-            tooltip: 'Admin',
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.person),
-            tooltip: 'Student',
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.person),
-            tooltip: 'Teacher',
-          )
-        ]),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: Column(children: [
+            Text(
+              'Choose your Role',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.1,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.admin_panel_settings),
+                    Text('Admin'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person),
+                    Text('Teacher'),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.person),
+                    Text('Student'),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
